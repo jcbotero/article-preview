78d9c2616d139eeb9ba303943c099ed0f275f31e
Juan Botero <jucaob10@hotmail.com>
2022-10-18 20:32:30 -0500

    done

---
+++ javas.js
@@ -0,0 +1,15 @@
+
+
+
+
+
+
+function red()  {
+
+    document.getElementById("redes").style.display= "";
+
+}; /* para que me muestre el div cuando tengo el mouse sobre */
+
+function out()  {
+    document.getElementById("redes").style.display= "none";
+} // para que me deje de mostrar el div cuando el mouse sale 
